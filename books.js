f61e33988a78841650e69556cd711a681ac9f138
jaketrent <trent.jake@gmail.com>
2015-09-09 06:05:53 -0600

    Add axios to request books

--- books.js
+++ books.js
@@ -1,16 +1,39 @@
+import axios from 'axios'
 import React from 'react'
 import Router from 'react-router'
 
 const { RouteHandler, Link } = Router
 
 export default class Books extends React.Component {
+  state = {
+    books: []
+  }
+  componentWillMount() {
+    axios({
+      url: 'http://data.jaketrent.com/api/v1/books'
+    }).then(res => {
+      this.setState({
+        books: res.data.books
+      })
+    })
+  }
+  renderBook(book) {
+    return (
+      <li key={book.id}>
+        <Link to="book" params={{ id: book.id }}>{book.title}</Link>
+      </li>
+    )
+  }
+  renderBooks(books) {
+    return books.map(this.renderBook)
+  }
   render() {
     return (
       <div>
         <h2>Books</h2>
-        <nav>
-          <Link to="book" params={{ id: 'drive' }}>Drive Book</Link>
-        </nav>
+        <ul>
+          {this.renderBooks(this.state.books)}
+        </ul>
         <RouteHandler />
       </div>
     )
